b82095385a51298f5ca79417dd8c283c381c53d6
Matthew Kemp <techmatt101@users.noreply.github.com>
2024-05-08 01:00:42 +0100

    improved tests + fixed bugs

--- tests/sopl.spec.js
+++ tests/sopl.spec.js
@@ -1,33 +1,119 @@
-import { expect, test, vi } from 'vitest';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
 import { TestAtdmInstance } from './test-helpers';
 
-const instance = new TestAtdmInstance();
-instance.init({
-    model: 'atdm-1012'
+describe('sopl action', () => {
+    let instance;
+    let sendCommandSpy;
+
+    beforeEach(() => {
+        instance = new TestAtdmInstance();
+        instance.init({
+            model: 'atdm-1012'
+        });
+        instance.DATA.operator_page[0]['fader_1_level'] = 50;
+        sendCommandSpy = vi.spyOn(instance, 'sendCommand');
+    });
+
+    test('set fader level', () => {
+        instance.actions['sopl'].callback({ options: { page: 1, fader: 1, level: 55 } });
+        expect(sendCommandSpy).toHaveBeenCalledWith('SOPL', 'S', '1,1,55');
+    });
+
+    test('increase fader level', async () => {
+        await instance.actions['sopl_increase'].callback({ options: { page: 1, fader: 1, steps: 1 } });
+        expect(sendCommandSpy).toHaveBeenCalledWith('SOPL', 'S', '1,1,51');
+    });
+
+    test('decrease fader level', async () => {
+        await instance.actions['sopl_decrease'].callback({ options: { page: 1, fader: 1, steps: 1 } });
+        expect(sendCommandSpy).toHaveBeenCalledWith('SOPL', 'S', '1,1,49');
+    });
+
 });
 
-test('set fader level', async () => {
-    const spy = vi.spyOn(instance, 'sendCommand');
+describe('sopl variable', () => {
+    let instance;
 
-    await instance.actions['sopl'].callback({ options: { page: 1, fader: 1, level: 55 }});
+    beforeEach(() => {
+        instance = new TestAtdmInstance();
+        instance.init({
+            model: 'atdm-1012'
+        });
+        instance.DATA.operator_page[0]['fader_1_level'] = 50;
+        instance.DATA.operator_page[0]['fader_2_level'] = 82;
+        instance.DATA.operator_page[0]['fader_3_level'] = 20;
+    });
 
-    expect(spy).toHaveBeenCalledWith('SOPL', 'S', '1,1,55');
-});
+    test('fader level', async () => {
+        const spy = vi.spyOn(instance, 'setVariableValues');
 
-test('increase fader level', async () => {
-    await instance.actions['sopl'].callback({ options: { page: 1, fader: 1, level: 45 }});
-    const spy = vi.spyOn(instance, 'sendCommand');
+        instance.checkVariables();
 
-    await instance.actions['sopl_increase'].callback({ options: { page: 1, fader: 1, steps: 1 }});
-
-    expect(spy).toHaveBeenCalledWith('SOPL', 'S', '1,1,46');
-});
-
-test('decrease fader level', async () => {
-    await instance.actions['sopl'].callback({ options: { page: 1, fader: 1, level: 45 }});
-    const spy = vi.spyOn(instance, 'sendCommand');
-
-    await instance.actions['sopl_decrease'].callback({ options: { page: 1, fader: 1, steps: 1 }});
-
-    expect(spy).toHaveBeenCalledWith('SOPL', 'S', '1,1,44');
+        expect(spy).toHaveBeenNthCalledWith(10, {
+            "gopl_1_1": 50,
+            "gopl_1_2": 82,
+            "gopl_1_3": 20,
+            "gopl_1_4": 0,
+            "gopl_1_5": 0,
+            "gopl_1_6": 0,
+            "gopl_1_7": 0,
+            "gopl_1_8": 0,
+            "gopl_2_1": 0,
+            "gopl_2_2": 0,
+            "gopl_2_3": 0,
+            "gopl_2_4": 0,
+            "gopl_2_5": 0,
+            "gopl_2_6": 0,
+            "gopl_2_7": 0,
+            "gopl_2_8": 0,
+            "gopl_3_1": 0,
+            "gopl_3_2": 0,
+            "gopl_3_3": 0,
+            "gopl_3_4": 0,
+            "gopl_3_5": 0,
+            "gopl_3_6": 0,
+            "gopl_3_7": 0,
+            "gopl_3_8": 0,
+            "gopl_4_1": 0,
+            "gopl_4_2": 0,
+            "gopl_4_3": 0,
+            "gopl_4_4": 0,
+            "gopl_4_5": 0,
+            "gopl_4_6": 0,
+            "gopl_4_7": 0,
+            "gopl_4_8": 0,
+            "gopl_5_1": 0,
+            "gopl_5_2": 0,
+            "gopl_5_3": 0,
+            "gopl_5_4": 0,
+            "gopl_5_5": 0,
+            "gopl_5_6": 0,
+            "gopl_5_7": 0,
+            "gopl_5_8": 0,
+            "gopl_6_1": 0,
+            "gopl_6_2": 0,
+            "gopl_6_3": 0,
+            "gopl_6_4": 0,
+            "gopl_6_5": 0,
+            "gopl_6_6": 0,
+            "gopl_6_7": 0,
+            "gopl_6_8": 0,
+            "gopl_7_1": 0,
+            "gopl_7_2": 0,
+            "gopl_7_3": 0,
+            "gopl_7_4": 0,
+            "gopl_7_5": 0,
+            "gopl_7_6": 0,
+            "gopl_7_7": 0,
+            "gopl_7_8": 0,
+            "gopl_8_1": 0,
+            "gopl_8_2": 0,
+            "gopl_8_3": 0,
+            "gopl_8_4": 0,
+            "gopl_8_5": 0,
+            "gopl_8_6": 0,
+            "gopl_8_7": 0,
+            "gopl_8_8": 0,
+        });
+    });
 });
